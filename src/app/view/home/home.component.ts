d0eea99dcb029beb6d0704a49f846a5a3e0f15fd
EmanuelJFM <emanuel.2211.noviembre@gmial.com>
2024-05-01 11:33:31 -0500

    feat: skills and contact section

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  
+  contactForm = new FormGroup({
+    nombre : new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    asunto: new FormControl('', Validators.required),
+    mensaje: new FormControl('', Validators.required)
+  });
 
+  downloadPdf() {
+    window.open('./assets/pdf/cv.pdf', '_blank');
+  }
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
 }
